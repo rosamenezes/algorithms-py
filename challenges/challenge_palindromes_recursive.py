3f9163e2e357839152ded2fe829ad8f6ee86a5ec
rosamenezes <g.rosamenezes1@gmail.com>
2023-07-14 12:02:07 -0300

    Linter

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,4 +7,5 @@ def is_palindrome_recursive(word, low_index, high_index):
         return True
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
+
 print(is_palindrome_recursive("REVIVER", 0, len("REVIVER") - 1))
