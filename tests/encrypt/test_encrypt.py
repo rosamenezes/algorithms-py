e57add002ea5317b4ad5b8622471912db2dbbc02
rosamenezes <g.rosamenezes1@gmail.com>
2023-07-14 10:59:39 -0300

    Challange (teste for encrypt_message)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(0, 1)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("banana", 3.5)
+
+    assert encrypt_message("message", 200) == "egassem"
+    assert encrypt_message("message", 2) == "egass_em"
+    assert encrypt_message("message", 3) == "sem_egas"
