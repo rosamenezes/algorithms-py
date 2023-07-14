e6e38b3bb71d49a7bc08764fafbfbef25fa6bdfc
rosamenezes <g.rosamenezes1@gmail.com>
2023-07-14 09:52:06 -0300

    Challange (studdy_schedule)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui. Iniciando Projeto"""
+    hour_repeat = 0
+    if target_time is None or permanence_period is None:
+        return None
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+        if period[0] <= target_time <= period[1]:
+            hour_repeat += 1
+    return hour_repeat
+
     raise NotImplementedError
