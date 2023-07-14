2253bd0b251a3cef8ca78372c956c0eb216bcbbd
rosamenezes <g.rosamenezes1@gmail.com>
2023-07-14 11:25:55 -0300

    Linter

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,6 +1,7 @@
 import pytest
 from challenges.challenge_encrypt_message import encrypt_message
 
+
 def test_encrypt_message():
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(0, 1)
